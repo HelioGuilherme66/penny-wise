eb9c2f1d299d0b1266cdc0301442606a41c92b44
HelioGuilherme66 <helioxentric@gmail.com>
2026-09-14 00:11:24 +0100

    Add initial setup for API tests with Jest

---
+++ backend/src/__tests__/basic.test.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const { createApp } = require('../app');
+const mongoose = require('mongoose');
+
+const app = createApp();
+
+describe('Basic tests', () => {
+  it('get root', async () => {
+    const res = await request(app).get('/');
+
+    expect(res.status).toBe(200);
+    console.log(res.body);
+    // expect(res.body).toMatch("Hello World");
+  });
+
+  it('get /api/health', async () => {
+    const res = await request(app).get('/api/health');
+
+    expect(res.status).toBe(200);
+    console.log(res.body);
+    expect(res.body).toMatchObject({ status: 'ok' });
+  });
+
+  it('get error 404', async () => {
+    const res = await request(app).get('/api/thisisnotthepageyouarelookingfor');
+
+    expect(res.status).toBe(404);
+    console.log(res.body);
+    expect(res.body).toMatchObject({ error: 'Not found' });
+  });
+});
